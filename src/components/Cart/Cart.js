2dc70a3767002ec74503f9e4dcf260e6dbff289c
Hamsa Aldrobi <hamsa.aldrobi@infinitaslearning.com>
2023-02-04 00:15:30 +0100

    feat: change logic when adding meal by the reducer dispatched function

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,18 +2,30 @@ import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-  console.log(cartCtx.totalAmount);
+
   const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items?.map((item) => (
-        <li key={item.id}>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemoveProp={cartItemRemoveHandler.bind(null, item.id)}
+          onAddProp={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
